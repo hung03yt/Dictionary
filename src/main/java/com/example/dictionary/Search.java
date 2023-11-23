9e727921ab02b0079ce15f8d9d5c8e9ed4af8f5f
hung03yt <hung03yt@gmail.com>
2023-11-23 14:11:24 +0700

    Updated 23/11

--- src/main/java/com/example/dictionary/Search.java
+++ src/main/java/com/example/dictionary/Search.java
@@ -1,69 +1,84 @@
 package com.example.dictionary;
 
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyEvent;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.input.MouseEvent;
 
-import java.io.IOException;
-import java.util.Collections;
-import java.util.List;
 
+// Đặt nhầm tên ->Online
 public class Search {
+    @FXML
+    private TextArea inputWord;
+    @FXML
+    private TextArea resultTxt;
+    @FXML
+    private TextField modeText;
+    @FXML
+    private ToggleButton modeButton;
 
-    @FXML
-    private ListView searchResult;
-    @FXML
-    private TextField searchWord;
-    @FXML
-    private TextArea vieResult;
-
+    public static String modeTrans;
     public void initialize() {
-        vieResult.setEditable(false);
+        modeTrans = "en-vi";
+        inputWord.setWrapText(true);
+        modeText.setEditable(false);
+        resultTxt.setWrapText(true);
+        resultTxt.setEditable(false);
+        modeText.setText("ENG to VIE");
     }
 
-    public void searchResult(KeyEvent event) throws IOException {
-        String search = searchWord.getText();
-        List<String> searchList = DictionaryCommandline.dictionarySearcher(search);
-        Collections.sort(searchList);
-        ObservableList<String> res = FXCollections.observableArrayList();
-        for (String word : searchList) {
-            res.add(word);
-        }
-        searchResult.setItems(res);
-        searchList.clear();
-        searchResult.getSelectionModel().selectIndices(0);
-    }
-
-    public void wordSearching(MouseEvent event){
-        Object temp = searchResult.getSelectionModel().getSelectedItem();
-        if (temp == null) {
-            //System.out.println("null check!");
+    public void setModeTrans(MouseEvent event) {
+        if (modeButton.isSelected()) {
+            modeTrans = "vi-en";
+            modeText.setText("VIE to ENG");
+            inputWord.setText("");
+            resultTxt.setText("");
         } else {
-            String tempTxt = DictionaryManagement.dictionaryLookup((String) temp);
-            vieResult.setText(DictionaryManagement.makeTrueForm(tempTxt));
+            modeTrans = "en-vi";
+            modeText.setText("ENG to VIE");
+            inputWord.setText("");
+            resultTxt.setText("");
         }
     }
 
-
-    public void speakerShow(MouseEvent event) {
-        //System.out.println("Speech");
-        DictionaryManagement.wordSpell(searchWord.getText());
-    }
-
-    public void spellWord(ActionEvent event) {
-        //System.out.println("Test Speech");
-        Object temp = searchResult.getSelectionModel().getSelectedItem();
-        if (temp == null) {
-            System.out.println("null check:");
-        } else {
-            DictionaryManagement.wordSpell(((String) temp));
+    public void onlineSearch(MouseEvent event) throws Exception {
+        String translateTxt = inputWord.getText().trim();
+        if (translateTxt != null && translateTxt.length() >0) {
+            if(modeTrans == "en-vi") {
+                String res = DictionaryManagement.getOnline("en", "vi", translateTxt);
+                resultTxt.setText(res);
+            } else {
+                String res = DictionaryManagement.getOnline("vi", "en", translateTxt);
+                resultTxt.setText(res);
+            }
         }
     }
+
+    public void voice(MouseEvent event) throws Exception {
+        String temp = inputWord.getText().trim();
+        if (temp.length() > 0 && temp != null) {
+            if (modeTrans.equals("en-vi")) {
+                VoiceAPI.getVoiceEng(temp);
+                VoiceAPI.play("eng_voice.wav");
+            } else {
+                VoiceAPI.getVoiceVie(temp);
+                VoiceAPI.play("vie_voice.wav");
+            }
+        }
+    }
+    public void voice2(MouseEvent event) throws Exception {
+        String temp = resultTxt.getText().trim();
+        if (temp.length() > 0 && temp != null) {
+            if (modeTrans.equals("en-vi")) {
+                VoiceAPI.getVoiceVie(temp);
+                VoiceAPI.play("vie_voice.wav");
+            } else {
+                VoiceAPI.getVoiceVie(temp);
+                VoiceAPI.play("eng_voice.wav");
+            }
+        }
+    }
+
 }
